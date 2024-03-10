abb9856fdaa88a5b2d5feb12f7f2e7bdea36029a
Fulminazzo <fulminazzo21@gmail.com>
2024-03-10 12:50:54 +0100

    Changed plugin variable to SimplyTabPlugin

--- src/main/java/net/simplyvanilla/simplytab/tab/TablistManager.java
+++ src/main/java/net/simplyvanilla/simplytab/tab/TablistManager.java
@@ -1,11 +1,9 @@
 package net.simplyvanilla.simplytab.tab;
 
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.simplyvanilla.simplytab.SimplyTabPlugin;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,20 +15,20 @@ import static net.kyori.adventure.text.minimessage.tag.Tag.inserting;
 import static net.kyori.adventure.text.minimessage.tag.resolver.TagResolver.resolver;
 
 public class TablistManager {
-    private final JavaPlugin javaPlugin;
+    private final SimplyTabPlugin plugin;
     private final TpsProvider tpsProvider;
 
-    public TablistManager(JavaPlugin plugin) {
-        this.javaPlugin = plugin;
+    public TablistManager(SimplyTabPlugin plugin) {
+        this.plugin = plugin;
         this.tpsProvider = SimplyTabPlugin.isFolia() ? new FoliaTpsProvider() : TpsProvider.DEFAULT;
 
-        this.javaPlugin.getServer().getAsyncScheduler().runAtFixedRate(this.javaPlugin, this::updateTablist, 0, 1, TimeUnit.SECONDS);
+        this.plugin.getServer().getAsyncScheduler().runAtFixedRate(this.plugin, this::updateTablist, 0, 1, TimeUnit.SECONDS);
     }
 
     private void updateTablist(ScheduledTask task) {
-        for (Player onlinePlayer : this.javaPlugin.getServer().getOnlinePlayers()) {
+        for (Player onlinePlayer : this.plugin.getServer().getOnlinePlayers()) {
             if (SimplyTabPlugin.isFolia()) {
-                onlinePlayer.getScheduler().run(this.javaPlugin, scheduledTask -> {
+                onlinePlayer.getScheduler().run(this.plugin, scheduledTask -> {
                     this.updateTablistForPlayer(onlinePlayer);
                 }, () -> {
                 });
@@ -48,13 +46,13 @@ public class TablistManager {
         TagResolver resolvers = resolver(
             getAudienceGlobalPlaceholders(player),
             tpsResolver,
-            resolver("mc-version", inserting(text(this.javaPlugin.getServer().getMinecraftVersion()))),
-            resolver("online-count", inserting(text(valueOf(this.javaPlugin.getServer().getOnlinePlayers().size()))))
+            resolver("mc-version", inserting(text(this.plugin.getServer().getMinecraftVersion()))),
+            resolver("online-count", inserting(text(valueOf(this.plugin.getServer().getOnlinePlayers().size()))))
         );
 
         player.sendPlayerListHeaderAndFooter(
-            miniMessage().deserialize(String.join("<newline>", this.javaPlugin.getConfig().getStringList("tab.header")), resolvers),
-            miniMessage().deserialize(String.join("<newline>", this.javaPlugin.getConfig().getStringList("tab.footer")), resolvers)
+            miniMessage().deserialize(String.join("<newline>", this.plugin.getConfig().getStringList("tab.header")), resolvers),
+            miniMessage().deserialize(String.join("<newline>", this.plugin.getConfig().getStringList("tab.footer")), resolvers)
         );
     }
 
@@ -63,9 +61,9 @@ public class TablistManager {
 
         String absoluteTps = valueOf((int) tps);
 
-        var placeholder = this.javaPlugin.getConfig().getString("tab.tps.*", "%value%");
-        if (this.javaPlugin.getConfig().contains("tab.tps.%s".formatted(absoluteTps))) {
-            placeholder = this.javaPlugin.getConfig().getString("tab.tps.%s".formatted(absoluteTps), "%value%");
+        var placeholder = this.plugin.getConfig().getString("tab.tps.*", "%value%");
+        if (this.plugin.getConfig().contains("tab.tps.%s".formatted(absoluteTps))) {
+            placeholder = this.plugin.getConfig().getString("tab.tps.%s".formatted(absoluteTps), "%value%");
         }
 
         String formattedTps = String.format("%.2f", tps);
